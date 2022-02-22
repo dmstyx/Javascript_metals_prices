12806a9ef7de4f654795c910d29fb65bff3c805d
TK <synova_matare@hotmail.co.uk>
2022-02-22 19:04:40 +0000

    Added input validation, currency conversion and SVG.

--- main.js
+++ main.js
@@ -19,37 +19,20 @@ btnAdd.addEventListener('click', () =>{
   let weight = metalWeight.value;
   let symbol = metalSymbol.value;
   let currency = metalCurrency.value;
-  let unit = metalUnit.value;
+  let unit = metalUnit.textContent;
 
-  populate(weight, symbol, currency, unit);
+  if (isNaN(weight)){
+    alert('Please input numeric characters only');
+  }else{
+    populate(weight, symbol, currency, unit);
+  };
 
 })
 
 async function populate(weight, symbol, currency, unit) {
 
-  // const metalsData = await fetch(`https://www.goldapi.io/api/${symbol}/${currency}`, requestOptions);
-  // const data = await metalsData.json();
-
-  const data = {
-    "timestamp": 1645019282,
-    "metal": "XAU",
-    "currency": "USD",
-    "exchange": "FOREXCOM",
-    "symbol": "FOREXCOM:XAUUSD",
-    "prev_close_price": 1853.51,
-    "open_price": 1853.51,
-    "low_price": 1850.38,
-    "high_price": 1859.65,
-    "open_time": 1644969600,
-    "price": 1856.67,
-    "ch": 3.16,
-    "chp": 0.17,
-    "ask": 1857,
-    "bid": 1856.22
-  }
-
-  console.log(data);
-
+  const metalsData = await fetch(`https://www.goldapi.io/api/${symbol}/${currency}`, requestOptions);
+  const data = await metalsData.json();
   populateResults(data, weight, symbol, currency, unit);
 }
 
@@ -71,17 +54,25 @@ function populateResults(obj, weight, symbol, currency, unit) {
     "XAG": "Silver",
     "XPT": "Platinum",
     "XPD": "Palladium"}
+  
+  let unitDict = {
+    "oz": 1,
+    "g": 0.03527396195,
+    "kg": 35.27396195,
+    "lb": 16,
+  }
 
+  let spotPrice = Intl.NumberFormat().format(obj['price'] * unitDict[unit])
+  let value = Intl.NumberFormat('en-GB', {style: 'currency', currency: `${currency}`}).format(obj['price'] * unitDict[unit] * weight)
 
   count+=1;
 
-  
   let template = `
                   <tr class="center aligned" id='${count}'>
                     <td>${symbolDict[symbol]} </td>
-                    <td>${weight}${unit}}</td>
-                    <td>${obj['price'] * unit}</td>
-                    <td>${currency} ${obj['price']}</td>
+                    <td>${weight}${unit}</td>
+                    <td>${spotPrice}</td>
+                    <td>${value}</td>
                     
                     <td><button onclick="myFunction(${count})" class="ui grey button">Remove</button></td>
                   </tr>`;
